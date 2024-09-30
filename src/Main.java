19cf7bf253bf26a4b703c8f631bf7e746487b68d
AlessioLosi <alessiolosi93@icloud.com>
2024-09-30 17:16:18 +0200

    s

--- src/Main.java
+++ src/Main.java
@@ -37,27 +37,10 @@ public class Main {
                     System.out.println(Arrays.toString(numerinteri));
                 }
             } catch (InputMismatchException e) {
-                System.out.println("!!!");
+                System.out.println("carattere non consentito");
                 scanner.nextLine();
             }
         }
 
-        Scanner scanner1 = new Scanner(System.in);
-        try {
-            System.out.println("Inserisci i km percorsi");
-            int km = scanner1.nextInt();
-            System.out.println("Inserisci i litri di carburante consumati");
-            int litri = scanner1.nextInt();
-
-            int Consumo = km / litri;
-            if (litri == 0) {
-                throw new ArithmeticException("non si può dividere per 0");
-            }
-            System.out.println("KM percorsi per litro:"+Consumo);
-            scanner1.close();
-        }catch (ArithmeticException e ){
-            System.out.println("inserisci un valore valido");
-            e.printStackTrace();
-        }
     }
 }
